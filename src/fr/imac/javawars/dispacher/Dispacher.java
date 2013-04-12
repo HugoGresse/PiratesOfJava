a8396dd78d4dcf6cfa29ab925cebce90e91df346
Hugo Gresse <hugo.gresse@gmail.com>
2013-04-12 17:00:18 +0200

    add some class in package

---
+++ src/fr/imac/javawars/dispacher/Dispacher.java
@@ -0,0 +1,17 @@
+package fr.imac.javawars.dispacher;
+
+public class Dispacher {
+
+	public Dispacher() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/fr/imac/javawars/player/Player.java
@@ -0,0 +1,17 @@
+package fr.imac.javawars.player;
+
+public class Player {
+
+	public Player() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
